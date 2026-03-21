0ce97858ebd3cf5da8cf8d85fc42ca42b762a7d7
GitHub Action <action@github.com>
2026-03-21 18:27:19 +0000

    Actualizar versión web - Release v1.5.10

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2301031921" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "424726498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
